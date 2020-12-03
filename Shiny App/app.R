daa8719f816502e659c38a5635c44d574fbc2cd0
Iszy Hirschtritt Licht <iszy@uchicago.edu>
2020-12-03 18:17:51 -0500

    Update league data and shiny

--- Shiny App/app.R
+++ Shiny App/app.R
@@ -3,29 +3,37 @@ library(shiny)
 library(tidyverse)
 library(shinythemes)
 library(ggthemes)
+library(scales)
 
 # Load data
 mario_data <- read_csv("Mario_Baseball_Data_update.csv")
+league_data <- read_csv("leaguewide_data.csv")
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("sandstone"),
-                titlePanel("Running Batting Averages"),
+                titlePanel("How do your favorite Mario Baseball characters compare?"),
                 sidebarLayout(
                   sidebarPanel(
                     
                     # Select players to plot
                     selectInput(inputId = "player_name1", label = strong("Player 1"),
-                                choices = unique(mario_data$player_name)),
+                                choices = sort(unique(mario_data$player_name))),
                     
                     selectInput(inputId = "player_name2", label = strong("Player 2"),
-                                choices = unique(mario_data$player_name))
+                                choices = sort(unique(mario_data$player_name))),
+                    
+                    #Add Shiny Logo
+                    img(src='shiny_logo.png', align = "top",  height = "100px")
                     
                   ),
                   
-                  # Output: Description, lineplot, and reference
+                  # Output: Lineplot, reference, and image
                   mainPanel(
                     plotOutput(outputId = "lineplot", height = "300px"),
-                    tags$a("Source: Collected Mario Superstar Baseball Data", target = "_blank")
+                    tags$a(href="https://github.com/Iszy-HL/Mario-Baseball-Stats/blob/master/Mario_Baseball_Data_update.csv",
+                            "Source: Collected Mario Superstar Baseball Data"),
+                    tags$b("Prepared by Iszy Hirschtritt Licht"),
+                    img(src='mario-baseball.png', align = "bottom",  height = "300px")
                   )
                 )
 )
@@ -50,15 +58,28 @@ server <- function(input, output) {
   # Create scatterplot object the plotOutput function is expecting
   output$lineplot <- renderPlot({
     ggplot()+
-      geom_line(selected_player_1(), mapping = aes(x = cum_at_bats, y = running_avg)) +
-      geom_line(selected_player_2(), mapping = aes(x = cum_at_bats, y = running_avg)) +
-      scale_x_continuous(breaks = scales::pretty_breaks(n = 20)) +
-      scale_y_continuous(breaks = scales::pretty_breaks(n = 20)) +
-      scale_color_discrete("Players") +
-      ylim(0, .5) +
+      geom_hline(yintercept = 0.334, linetype = "dashed", color = "black", size=1) +
+      geom_line(selected_player_1(), mapping = aes(x = cum_at_bats, y = running_avg, 
+                                                   color = player_name)) +
+      geom_line(selected_player_2(), mapping = aes(x = cum_at_bats, y = running_avg, 
+                                                   color = player_name)) +
+      scale_y_continuous(labels = scales::percent_format(accuracy = .001)) +
+      # scale_x_continuous(breaks = scales::pretty_breaks(n = 20)) +
+      # scale_y_continuous(breaks = scales::pretty_breaks(n = 20)) +
+      ylim(0, .750) + 
+      xlim(0, 350) +
       labs(title = "Running Batting Average",
+           subtitle = "League Average: 0.334",
            x = "At Bats", y ="Batting Average") +
-      theme_economist() 
+      scale_color_discrete(name = "Players:", ) + 
+      theme_solarized() +
+      theme(title = element_text(size = 20), 
+            legend.title = element_text(size = 20), 
+            legend.text = element_text(size = 15),
+            legend.key.size = unit(3,"line"), 
+            legend.key=element_rect(fill='lightblue')) + 
+    guides(colour = guide_legend(override.aes = list(size=2, stroke=1.5)))
+      
   })
   
 }
